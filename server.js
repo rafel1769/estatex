f6be880e5e740dfff1e043d7df2533f4ee55c8a2
rafel <you@gmail.com>
2026-04-12 23:54:41 +0500

    fix server

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
-// чтобы сервер понимал JSON
 app.use(express.json());
 
 // тестовый маршрут
 app.get("/", (req, res) => {
-  res.send("🚀 Estatex работает!");
+  res.send("🚀 ESTATEX работает!");
 });
 
-// пример API
-app.get("/api", (req, res) => {
-  res.json({ message: "API работает" });
-});
-
-// ВАЖНО для Render
+// ПОРТ ДЛЯ RENDER
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
-
-
-
 
 
 
